0d3d98ccb6e6a67bfc57358711e68ee2f5009383
Silvia <syl_peke14@hotmail.com>
2023-05-20 13:33:47 -0500

    Implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
