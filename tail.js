771c79ca6e9aab313053a4fa062a17de6ac1d8b5
Silvia <syl_peke14@hotmail.com>
2023-05-20 15:08:28 -0500

    Implement Tail

--- tail.js
+++ tail.js
@@ -1,3 +1,13 @@
+const assertEqual = function (actual, expected) {
+    if (actual === expected) {
+        console.log(`Assertion passed: ${actual} === ${expected}`);
+    } else {
+        console.log(`Assertion failed: ${actual} !== ${expected}`);
+    }
+};
+
 const tail = function (array) {
     return array.slice(1);
 };
+
+assertEqual(tail, ["Lighthouse", "Labs"]);
