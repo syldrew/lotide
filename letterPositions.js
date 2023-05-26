3d9119745ed7ede44b469dfadf8b95024071a2be
syldrew <syl_peke14@hotmail.com>
2023-05-26 11:22:33 -0500

    Implement letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const assertArraysEqual = function (array1, array2) {
+    if (eqArrays(array1, array2)) {
+        console.log(`Assertion Passed: ${array1} === ${array2}`);
+    } else {
+        console.log(`Assertion Failed: ${array1} !== ${array2}`);
+    }
+};
+
+const eqArrays = function (array1, array2) {
+    if (array1.length !== array2.length) {
+        return false;
+    }
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const letterPositions = function (sentence) {
+    const results = {};
+    for (let i = 0; i < sentence.length; i++) {
+        let letter = sentence[i];
+        if (letter === ' ') {
+            continue;
+        } else if (!results[letter]) {
+            results[letter] = [i];
+
+        } else {
+            results[letter].push(i);
+        }
+
+    } return results;
+};
+console.log(letterPositions("This is a test."))
+
+
+assertArraysEqual(letterPositions("hello").e, [1]);
