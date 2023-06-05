9d41b9e112ba11c9e8ab0d8b0e09d7e4e9c00548
SilviaChavez <syl_peke14@hotmail.com>
2023-06-05 11:05:25 -0500

    Update map.js

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 const words = ["ground", "control", "to", "major", "tom"];
-<<<<<<< HEAD
+
 const map = function (array, callback) {
     const results = [];
     for (let item of array) {
@@ -9,16 +9,5 @@ const map = function (array, callback) {
 }
 
 const results1 = map(words, word => word[0]);
-console.log(results1)
-=======
-const map = function(array, callback) {
-  const results = [];
-  for (let item of array) {
-    results.push(callback(item));
-  }
-  return results;
-}
+console.log(results1);
 
-const results1 = map(words, word => word[0]);
-console.log(results1)
->>>>>>> c242b66492058f760fd47f69e4b0157e80819cb8
