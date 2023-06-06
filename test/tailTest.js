4ad7af2ef63de2dc373ab95fa491abbbf0497c23
syldrew <syl_peke14@hotmail.com>
2023-06-06 17:25:00 -0500

    "M & C"

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,15 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+//const assertEqual = require('../assertEqual');
 const tail   = require('../tail');
 
 
+describe("#tail", () => {
+  
+    it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+      assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+    });
+  });
 
-assertEqual(tail([5, 6, 7]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(tail([5, 6, 7]), 5);
+//assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
 
